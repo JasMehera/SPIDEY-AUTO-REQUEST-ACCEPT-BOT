fe259070f9d6293db4e6b8fc7035599be6e9a41f
JasMehera <ichigobotam1@gmail.com>
2025-05-01 20:00:51 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@
 
 class script(object):
 
-    START_MSG = """<b>🦊 Hᴇʟʟᴏ {}!\n\nI'ᴍ ᴀɴ ᴀᴜᴛᴏ-ᴀᴘᴘʀᴏᴠᴇ [ᴀᴅᴍɪɴ](https://t.me/hacker_x_official_777) [Jᴏɪɴ Rᴇǫᴜᴇsᴛs](https://t.me/telegram/153) ʙᴏᴛ.\nI ᴄᴀɴ ᴀᴘᴘʀᴏᴠᴇ ᴜsᴇʀs ɪɴ Gʀᴏᴜᴘs/Cʜᴀᴛs. Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛ ᴀɴᴅ [ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴛᴏ ᴀᴅᴍɪɴ]()\n\n__ᴘᴏᴡᴇʀᴇᴅ ʙʏ: [@Hᴀᴄᴋᴇʀ_x_ᴏғғɪᴄɪᴀʟ_𝟽𝟽𝟽](https://t.me/hacker_x_official_777)__</b>"""
+    START_MSG = """<b>🦊 Hᴇʟʟᴏ {}!\n\nI'ᴍ ᴀɴ ᴀᴜᴛᴏ-ᴀᴘᴘʀᴏᴠᴇ [ᴀᴅᴍɪɴ](https://t.me/Union_Botss) [Jᴏɪɴ Rᴇǫᴜᴇsᴛs](https://t.me/telegram/153) ʙᴏᴛ.\nI ᴄᴀɴ ᴀᴘᴘʀᴏᴠᴇ ᴜsᴇʀs ɪɴ Gʀᴏᴜᴘs/Cʜᴀᴛs. Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀᴛ ᴀɴᴅ [ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴛᴏ ᴀᴅᴍɪɴ]()\n\n__ᴘᴏᴡᴇʀᴇᴅ ʙʏ: [@Union_Botss](https://t.me/Union_Botss)__</b>"""
     
     DISCLAIMER_TXT = """
 <b>      ᴀᴜᴛᴏ ʀᴇǫᴜᴇsᴛ ᴀᴄᴄᴇᴘᴛ ʙᴏᴛ
@@ -12,7 +12,7 @@ class script(object):
    
   ᴛʜɪs ʙᴏᴛ ɪs ᴀ ᴘᴜʙʟɪᴄʟʏ ᴀᴠᴀɪʟᴀʙʟᴇ ᴛᴏᴏʟ ᴅᴇsɪɢɴᴇᴅ ᴛᴏ ᴀssɪsᴛ ᴄʜᴀɴɴᴇʟ ᴏᴡɴᴇʀs ʙʏ ᴀᴜᴛᴏᴍᴀᴛɪɴɢ ᴛʜᴇ ᴘʀᴏᴄᴇss ᴏғ ᴀᴄᴄᴇᴘᴛɪɴɢ ʀᴇǫᴜᴇsᴛs ᴏɴ ᴛʜᴇɪʀ ᴄʜᴀɴɴᴇʟs. Iᴛ ɪs ɪɴᴛᴇɴᴅᴇᴅ ᴛᴏ ɪɴᴄʀᴇᴀsᴇ ᴇғғɪᴄɪᴇɴᴄʏ ᴀɴᴅ ᴇɴʜᴀɴᴄᴇ ᴛʜᴇ ʀᴇǫᴜᴇsᴛ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ᴇxᴘᴇʀɪᴇɴᴄᴇ. ᴀʟʟ ʀɪɢʜᴛs ᴛᴏ ᴛʜᴇ ᴏʀɪɢɪɴᴀʟ sᴏғᴛᴡᴀʀᴇ ᴀɴᴅ ʀᴇʟᴀᴛᴇᴅ ᴄᴏɴᴛᴇɴᴛ ᴀʀᴇ ᴛʜᴇ ᴘʀᴏᴘᴇʀᴛʏ ᴏғ ᴛʜᴇɪʀ ʀᴇsᴘᴇᴄᴛɪᴠᴇ ᴄᴏᴘʏʀɪɢʜᴛ ʜᴏʟᴅᴇʀs. Tʜɪs ʙᴏᴛ ɪs ɴᴏᴛ ᴀғғɪʟɪᴀᴛᴇᴅ ᴡɪᴛʜ, ᴇɴᴅᴏʀsᴇᴅ ʙʏ, ᴏʀ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ ʙʏ ᴛᴇʟᴇɢʀᴀᴍ , ᴜɴʟᴇss ᴏᴛʜᴇʀᴡɪsᴇ sᴘᴇᴄɪғɪᴇᴅ. Usᴇ ᴏғ ᴛʜɪs ʙᴏᴛ ᴅᴏᴇs ɴᴏᴛ ɢʀᴀɴᴛ ᴏᴡɴᴇʀsʜɪᴘ ʀɪɢʜᴛs ᴛᴏ ᴛʜᴇ sᴏғᴛᴡᴀʀᴇ ᴏʀ ᴀssᴏᴄɪᴀᴛᴇᴅ ɪɴᴛᴇʟʟᴇᴄᴛᴜᴀʟ ᴘʀᴏᴘᴇʀᴛʏ. ʙʏ ᴜsɪɴɢ ᴛʜɪs ʙᴏᴛ, ʏᴏᴜ ᴀᴄᴋɴᴏᴡʟᴇᴅɢᴇ ᴀɴᴅ ᴀɢʀᴇᴇ ᴛᴏ ᴄᴏᴍᴘʟʏ ᴡɪᴛʜ ᴀʟʟ ʀᴇʟᴇᴠᴀɴᴛ ᴛᴇʀᴍs ᴏғ sᴇʀᴠɪᴄᴇ ᴀɴᴅ ᴜsᴀɢᴇ ᴘᴏʟɪᴄɪᴇs ᴏғ ᴛʜᴇ ᴘʟᴀᴛғᴏʀᴍs ʏᴏᴜ ɪɴᴛᴇʀᴀᴄᴛ ᴡɪᴛʜ. ғᴏʀ ɪɴǫᴜɪʀɪᴇs ᴏʀ ᴄᴏɴᴄᴇʀɴs, ᴘʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ ᴛʜᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ ᴏʀ ʀᴇғᴇʀ ᴛᴏ ᴛʜᴇ ʀᴇʟᴇᴠᴀɴᴛ ᴘʟᴀᴛғᴏʀᴍ's ᴛᴇʀᴍs ᴏғ sᴇʀᴠɪᴄᴇ. 
 
-<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/Hacker_x_official_777'>ʜᴀᴄᴋᴇʀ_x_ᴏꜰꜰɪᴄɪᴀʟ_777</a></b>
+<blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/Union_Botss'>ᴜɴɪᴏɴ ʙᴏᴛs</a></b>
  """
     
     LOGO_MSG ="""
@@ -36,13 +36,13 @@ class script(object):
 
 ᴀɴᴅ ᴜsɪɴɢ ᴀɴᴅ <a href='https://cloud.mongodb.com'>ᴍᴏɴɢᴏ</a> ᴀs ᴅᴀᴛᴀʙᴀsᴇ.
 
-» Sᴜʙsᴄʀɪʙᴇ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ : <a href='https://youtube.com/@spidey_official_777'>Sᴘɪᴅᴇʏ ᴏғғɪᴄɪᴀʟ</a>
+» Sᴜʙsᴄʀɪʙᴇ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/Union_Botss'>ᴜɴɪᴏɴ ʙᴏᴛs</a>
 
-ꜱᴘɪᴅᴇʏ  ɪs ʟɪᴄᴇɴsᴇᴅ ᴜɴᴅᴇʀ ᴛʜᴇ <a href='https://github.com/Spideyofficial777/Spidey-MtLisance/blob/main/LICENSE'>ᴍɪᴛ ʟɪᴄᴇɴsᴇ</a> .
+ꜱᴘɪᴅᴇʏ  ɪs ʟɪᴄᴇɴsᴇᴅ ᴜɴᴅᴇʀ ᴛʜᴇ <a href='https://github.com/'>ᴍɪᴛ ʟɪᴄᴇɴsᴇ</a> .
 
-© 𝟸𝟶𝟸𝟻 | <a href=https://t.me/SPIDEYOFFICIAL777'>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> , ᴀʟʟ ʀɪɢʜᴛs ʀᴇsᴇʀᴠᴇᴅ.
+© 𝟸𝟶𝟸𝟻 | <a href=https://t.me/Union_Botss'>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> , ᴀʟʟ ʀɪɢʜᴛs ʀᴇsᴇʀᴠᴇᴅ.
   
-  <blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/Hacker_x_official_777'>ʜᴀᴄᴋᴇʀ_x_ᴏꜰꜰɪᴄɪᴀʟ_777</a></blockquote></b>"""
+  <blockquote>🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/Union_Botss'>ᴜɴɪᴏɴ ʙᴏᴛs</a></blockquote></b>"""
      
 # Don't Remove Credit @spideyofficial777
 # Subscribe YouTube Channel For Amazing Bot @spidey_official_777
@@ -50,7 +50,7 @@ class script(object):
    
     ALERT_MSG = """ ⚡️ ʟɪᴠᴇ sʏsᴛᴇᴍ sᴛᴀᴛᴜs ⚡️\n\n❂ ʀᴀᴍ ●●●●●●●◌◌◌\n✇ ᴄᴘᴜ ●●●●●●●◌◌◌\n✪ ᴅᴀᴛᴀ ᴛʀᴀꜰɪᴄs ●●●●◌◌◌◌◌◌ 🛰\n\nᴠ𝟷.𝟶 [sᴛᴀʙʟᴇ] """
     
-    ABOUT_TXT = """<blockquote><b>‣ ᴍʏ ɴᴀᴍᴇ : <a href='https://telegram.me/Spidey_gaming_official_bot'>sᴘɪᴅᴇʏ_ɢᴀᴍɪɴɢ_ᴏғғɪᴄɪᴀʟ_ʙᴏᴛ</a>\n‣ ᴄʀᴇᴀᴛᴏʀ : <a href='https://telegram.me/Hacker_x_official_777'>ʜᴀᴄᴋᴇʀ_x_ᴏꜰꜰɪᴄɪᴀʟ_777</a>\n‣ ʟɪʙʀᴀʀʏ : ᴘʏʀᴏɢʀᴀᴍ\n‣ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ\n‣ ᴅᴀᴛᴀ ʙᴀsᴇ : ᴍᴏɴɢᴏ ᴅʙ\n‣ ʜᴏsᴛᴇᴅ ᴏɴ  : ᴀʟʟ ᴡᴇʙ\n‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ𝟷.𝟶 [sᴛᴀʙʟᴇ]</b></blockquote>"""    
+    ABOUT_TXT = """<blockquote><b>‣ ᴍʏ ɴᴀᴍᴇ : <a href='https://telegram.me/Pending_Request_Auto_Accept_Bot'>ᴀᴜᴛᴏ ᴀᴄᴄᴇᴘᴛ ʙᴏᴛ</a>\n‣ ᴄʀᴇᴀᴛᴏʀ : <a href='https://telegram.me/Union_Botss'>ᴜɴɪᴏɴ ʙᴏᴛs</a>\n‣ ʟɪʙʀᴀʀʏ : ᴘʏʀᴏɢʀᴀᴍ\n‣ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ\n‣ ᴅᴀᴛᴀ ʙᴀsᴇ : ᴍᴏɴɢᴏ ᴅʙ\n‣ ʜᴏsᴛᴇᴅ ᴏɴ  : ᴀʟʟ ᴡᴇʙ\n‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ𝟷.𝟶 [sᴛᴀʙʟᴇ]</b></blockquote>"""    
     
     CHANNELS = """
 <b>๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ɢɪᴠᴇɴ ʙᴇʟᴏᴡ ᴛᴏ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ɢᴇᴛ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴜs.</b>"""
@@ -62,9 +62,9 @@ class script(object):
     OWNER_TEXT = """
 <b>━━━━━━━━━━━━━━━━━━━━━
 🚀 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀɴ ᴇʟɪᴛᴇ ᴅᴇᴠ 🚀  
-👑 Oᴡɴᴇʀ:  [@ʜᴀᴄᴋᴇʀ_x_ᴏғғɪɪᴀʟ_𝟽𝟽𝟽](t.me/hacker_x_official_777)  
-🔗 Tᴇʟᴇɢʀᴀᴍ: [Oᴡɴᴇʀ](t.me/hacker_x_official_777)  
-🌍 GɪᴛHᴜʙ: [@ꜱᴘɪᴅᴇʏᴏғғɪᴄɪᴀʟ𝟽𝟽𝟽](github.com/spideyofficial777)
+👑 Oᴡɴᴇʀ:  [@Union_Botss](t.me/Union_Botss)  
+🔗 Tᴇʟᴇɢʀᴀᴍ: [Oᴡɴᴇʀ](t.me/Union_Botss)  
+🌍 sᴜᴘᴘᴏʀᴛ: [@Union_Botss](t.me/Union_Botss)
 Cʟɪᴄᴋ Hᴇʀᴇ
 ━━━━━━━━━━━━━━━━━━━━━
 🔥 Iɴɴᴏᴠᴀᴛɪɴɢ ᴛʜᴇ Fᴜᴛᴜʀᴇ, Oɴᴇ Cᴏᴅᴇ ᴀᴛ ᴀ Tɪᴍᴇ! 🔥
@@ -77,7 +77,7 @@ Cʟɪᴄᴋ Hᴇʀᴇ
 ≈  ɢʀᴏᴜᴘ: {chat_title} </b>"""
 
 
-    GSTART_TXT = """<b>ʜᴇʏ {},\n\nɪ ᴀᴍ ᴛʜᴇ ᴍᴏꜱᴛ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏ ʀᴇǫᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ʙᴏᴛ ᴡɪᴛʜ ᴘʀᴇᴍɪᴜᴍ ꜰᴇᴀᴛᴜʀᴇꜱ.\n\nᴍᴀɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/Hacker_x_official_777'>ꜱᴘɪᴅᴇʏ</a></b>"""
+    GSTART_TXT = """<b>ʜᴇʏ {},\n\nɪ ᴀᴍ ᴛʜᴇ ᴍᴏꜱᴛ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏ ʀᴇǫᴜᴇꜱᴛ ᴀᴄᴄᴇᴘᴛ ʙᴏᴛ ᴡɪᴛʜ ᴘʀᴇᴍɪᴜᴍ ꜰᴇᴀᴛᴜʀᴇꜱ.\n\nᴍᴀɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://telegram.me/Union_Botss'></a></b>"""
 
 
 # Don't Remove Credit @spideyofficial777
